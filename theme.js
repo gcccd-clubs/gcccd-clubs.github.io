35f38beba8f28ad8955b280e9874674f6a9319bc
smarttarded <khiemgluong@gmail.com>
2023-03-28 08:37:43 -0700

    Folder restructure, header.js, arabic-club tmplate

--- theme.js
+++ theme.js
@@ -1,4 +1,3 @@
-//Prepares the document (check for jQuery). Remember to load jQuery in the html file BEFORE running any scripts with jQuery.
 $(document).ready(function () {
 
     // Get the first header iframe element with the id of header
@@ -11,7 +10,7 @@ $(document).ready(function () {
     //We get the contents of the footer the same way, since we also want to change its value when we toggle dark/light mode
     var footer = $('#footer');
     var footerI = footer.contents()[0];
-    
+
     //We find the icon id, which is the light/dark mode icon, and set a function that runs whenever it is clicked
     $(headerI).find('#icon').click(function () {
         event.preventDefault();
@@ -20,7 +19,7 @@ $(document).ready(function () {
         var logo =  $(headerI).find('#clubLogo');
 
         //Toggle between the two images, this checks if the image is equal to moon.png (=== operator), then you change it to sun.png
-        if (icon.attr('src') === '/grossmont/computer-science-and-cybersecurity-organization/media/moon.png') {
+        if (icon.attr('src') === '/media/moon.png') {
             darkmode();
         //But if the icon is not moon.png, THEN we set it to moon, and change all the properties that we changed earlier back
         } else {
@@ -30,56 +29,34 @@ $(document).ready(function () {
         //*** FUNCTIONS TO LESSEN CODE ***/
 
         function darkmode(){
-            icon.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/whitesun.png');
+            icon.attr('src', '/media/whitesun.png');
             logo.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/dark_adjusted logo.png')
             
             //sets the body of the header to #333333
-            $('body').css('background-color', '#8380d0');
+            $('body').css('background-color', 'var(--primary-color)');
             //sets the header element of the header.html file to #333333;
             $(headerI).find('header').css('background-color', '#8380d0');
             
             activePage.css('background-color', 'var(--secondary-color)'); 
 
             //not sure about the most optimal way to do this, but it works
-            activePage.find('h1').css('color','var(--primary-color)'); 
-            activePage.find('h2').css('color','var(--primary-color)'); 
-            activePage.find('h3').css('color','var(--primary-color)'); 
-            activePage.find('h4').css('color','var(--primary-color)'); 
-            activePage.find('p').css('color','var(--primary-color)');
+            activePage.find('h1, h2, h3, h4, p, a, li').css('color', 'var(--primary-color)');
 
-            activePage.find('a').css('color','var(--primary-color)'); 
-            activePage.find('li').css('color','var(--primary-color)'); 
-
-            $(footerI).find('body').css('background-color', 'var(--secondary-color)');
-            $(footerI).find('footer').css('background-color', 'var(--secondary-color)');//secondary color = #171717 (black)
-            $(footerI).find('p').css('color', 'var(--primary-color)');//primary color = white
-            $(footerI).find('a').css('color', 'var(--primary-color)');
+            $(footerI).find('body, footer').css('background-color', 'var(--secondary-color)');//secondary color = #171717 (black)
+            $(footerI).find('p, a').css('color', 'var(--primary-color)');//primary color = white
         } 
         function lightmode(){
-            icon.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/moon.png');
+            icon.attr('src', '/media/moon.png');
             logo.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/adjusted logo.png')
 
-            $('body').css('background-color', '#89B0E4'); 
+            $('body').css('background-color', 'var(--primary-color)'); 
             $(headerI).find('header').css('background-color', '#89B0E4');
 
-        
+            activePage.find('h1, h2, h3, h4, p, a, li').css('color', 'var(--secondary-color)');            
 
-            activePage.css('background-color', 'var(--primary-color)');
-            activePage.find('h1').css('color','var(--secondary-color)'); 
-            activePage.find('h2').css('color','var(--secondary-color)'); 
-            activePage.find('h3').css('color','var(--secondary-color)'); 
-            activePage.find('h4').css('color','var(--secondary-color)'); 
-            activePage.find('p').css('color','var(--secondary-color)');
-            
-            activePage.find('a').css('color','var(--secondary-color)'); 
-            activePage.find('li').css('color','var(--secondary-color)'); 
-            
-
-            $(footerI).find('body').css('background-color', 'var(--primary-color)');
+            $(footerI).find('body, footer').css('background-color', 'var(--primary-color)');
             //@suldanaa see if you can select the footer and header and change the color back, and the font color
-            $(footerI).find('footer').css('background-color', 'var(--primary-color)');
-            $(footerI).find('p').css('color', 'var(--secondary-color)');
-            $(footerI).find('a').css('color', 'var(--secondary-color)');
+            $(footerI).find('p, a').css('color', 'var(--secondary-color)');
         }
     });
 });
