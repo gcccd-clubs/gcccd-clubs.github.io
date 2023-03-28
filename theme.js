16f655c5eb3315230e499cec818a569744ec706d
suldanaa <suldanaafrah@gmail.com>
2023-03-27 21:04:37 -0700

    Makes header smaller and updates dark mode colors

--- theme.js
+++ theme.js
@@ -11,24 +11,36 @@ $(document).ready(function () {
     //We get the contents of the footer the same way, since we also want to change its value when we toggle dark/light mode
     var footer = $('#footer');
     var footerI = footer.contents()[0];
+    
     //We find the icon id, which is the light/dark mode icon, and set a function that runs whenever it is clicked
     $(headerI).find('#icon').click(function () {
         event.preventDefault();
         //Set the icon id to a variable
         var icon = $(headerI).find('#icon');
+        var logo =  $(headerI).find('#clubLogo');
 
         //Toggle between the two images, this checks if the image is equal to moon.png (=== operator), then you change it to sun.png
         if (icon.attr('src') === '/grossmont/computer-science-and-cybersecurity-organization/media/moon.png') {
-            icon.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/sun.png');
+            darkmode();
+        //But if the icon is not moon.png, THEN we set it to moon, and change all the properties that we changed earlier back
+        } else {
+           lightmode();
+        }
 
+        //*** FUNCTIONS TO LESSEN CODE ***/
+
+        function darkmode(){
+            icon.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/whitesun.png');
+            logo.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/dark_adjusted logo.png')
+            
             //sets the body of the header to #333333
-            //$('body').css('background-color', '#333333');
+            $('body').css('background-color', '#8380d0');
             //sets the header element of the header.html file to #333333;
-            //$(headerI).find('header').css('background-color', '#333333');
+            $(headerI).find('header').css('background-color', '#8380d0');
             
             activePage.css('background-color', 'var(--secondary-color)'); 
-            //not sure about the most optimal way to do this, but it works
 
+            //not sure about the most optimal way to do this, but it works
             activePage.find('h1').css('color','var(--primary-color)'); 
             activePage.find('h2').css('color','var(--primary-color)'); 
             activePage.find('h3').css('color','var(--primary-color)'); 
@@ -42,13 +54,16 @@ $(document).ready(function () {
             $(footerI).find('footer').css('background-color', 'var(--secondary-color)');//secondary color = #171717 (black)
             $(footerI).find('p').css('color', 'var(--primary-color)');//primary color = white
             $(footerI).find('a').css('color', 'var(--primary-color)');
-
-        //But if the icon is not moon.png, THEN we set it to moon, and change all the properties that we changed earlier back
-        
-        } else {
+        } 
+        function lightmode(){
             icon.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/moon.png');
+            logo.attr('src', '/grossmont/computer-science-and-cybersecurity-organization/media/adjusted logo.png')
 
-            //$('body').css('background-color', 'var(--primary-color)'); 
+            $('body').css('background-color', '#89B0E4'); 
+            $(headerI).find('header').css('background-color', '#89B0E4');
+
+        
+
             activePage.css('background-color', 'var(--primary-color)');
             activePage.find('h1').css('color','var(--secondary-color)'); 
             activePage.find('h2').css('color','var(--secondary-color)'); 
